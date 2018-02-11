a06f8f479756cc754d0ffdab9a4d84f3424e326b
momka45 <momka45@localhost.localdomain>
2018-02-12 00:00:07 +0200

    Added offline csv with locations and circle markers

--- map_from_file.py
+++ map_from_file.py
@@ -1,8 +1,10 @@
 from collections import defaultdict
 from itertools import groupby
+from math import isnan
 import re
 import folium
 import requests
+import os
 
 
 def read_file(path):
@@ -15,6 +17,21 @@ def read_file(path):
     for i in f.read().strip().split("\n"):
         lines_list.append(i)
     return lines_list
+
+
+def read_locations(path):
+    """
+    (str) -> dict
+    Return dict where place is a key and tuple
+    with latitude and longitude is value
+    """
+    loc_dict = dict()
+    f = open(path, encoding="utf-8", errors="ignore")
+    f.readline();
+    for line in f.readlines():
+        place, lat, lng = line.strip("\n").split("\t")
+        loc_dict[place] = [float(lat), float(lng)]
+    return loc_dict
 
 
 def film_dict(lines_list, year):
@@ -65,41 +82,79 @@ def create_map(year):
     print("Finding films in file")
     f_dict = film_dict(read_file("locations.list"), year)
     print("Found films")
-
+    loc_dict = dict()
+    if ("locations.tsv" in os.listdir()):
+        loc_dict = read_locations("locations.tsv")
+    markers_count = len(f_dict)
+    added_markers = 0
     for address, n_list in f_dict.items():
-        print("Getting locations...")
-        lat_lng = get_location(address)
+        print("Map is ready for %.2f %%" % round(added_markers / markers_count * 100, 2))
+        if address in loc_dict.keys():
+            lat_lng = loc_dict[address]
+        else:
+            print("Need to geolocate")
+            lat_lng = get_location(address)
+        if isnan(lat_lng[0]) or isnan(lat_lng[1]):
+            lat_lng = get_location(address)
         if lat_lng:
-            for feature_group, names in groupby(n_list, key=lambda x: x[1]):
-                marker_name = ""
-                for name, f_g in names:
-                    marker_name = marker_name + name + ';\n'
-                marker_name = marker_name.replace("'", "\"", len(marker_name))
-                if feature_group == 1:
-                    studio_group.add_child(folium.Marker(
-                        location=lat_lng,
-                        popup=marker_name,
-                        icon=folium.Icon(color='red')))
-                elif feature_group == 2:
-                    tv_group.add_child(folium.Marker(
-                        location=lat_lng,
-                        popup=marker_name,
-                        icon=folium.Icon(color='purple')))
-                elif feature_group == 3:
-                    v_group.add_child(folium.Marker(
-                        location=lat_lng,
-                        popup=marker_name,
-                        icon=folium.Icon(color='blue')))
-                else:
-                    others_group.add_child(folium.Marker(
-                        location=lat_lng,
-                        popup=marker_name,
-                        icon=folium.Icon(color='green')))
+            try:
+                for feature_group, names in groupby(n_list, key=lambda x: x[1]):
+                    marker_name = ""
+                    for name, f_g in names:
+                        marker_name = marker_name + name + ';\n'
+                        marker_name = marker_name.replace("'", "\"", len(marker_name))
+                    names_count = marker_name.count(";")
+                    if names_count < 3:
+                        fill_color = "green"
+                    elif names_count < 6:
+                        fill_color = "yellow"
+                    else:
+                        fill_color = "red"
+                    if feature_group == 1:
+                        studio_group.add_child(folium.CircleMarker(
+                            radius=5,
+                            color="pink",
+                            fill=True,
+                            fill_color=fill_color,
+                            fill_opacity=0.5,
+                            location=lat_lng,
+                            popup=marker_name))
+                    elif feature_group == 2:
+                        tv_group.add_child(folium.CircleMarker(
+                            radius=5,
+                            color="purple",
+                            fill=True,
+                            fill_color=fill_color,
+                            fill_opacity=0.5,
+                            location=lat_lng,
+                            popup=marker_name))
+                    elif feature_group == 3:
+                        v_group.add_child(folium.CircleMarker(
+                            radius=5,
+                            color="blue",
+                            fill=True,
+                            fill_color=fill_color,
+                            fill_opacity=0.5,
+                            location=lat_lng,
+                            popup=marker_name))
+                    else:
+                        others_group.add_child(folium.CircleMarker(
+                            radius=5,
+                            color="grey",
+                            fill=True,
+                            fill_color=fill_color,
+                            fill_opacity=0.5,
+                            location=lat_lng,
+                            popup=marker_name))
+            except ValueError:
+                print(address, loc_dict[address])
+            added_markers += 1
 
-    print("Add markers to map")
+    print("Added markers to map")
     essential_map.add_child(studio_group)
     essential_map.add_child(tv_group)
     essential_map.add_child(v_group)
     essential_map.add_child(others_group)
     essential_map.add_child(folium.LayerControl())
     essential_map.save("Map.html")
+    print("Map is done")
